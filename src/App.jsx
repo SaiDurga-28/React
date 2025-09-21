760387da0c4385a7c57c1b542eef0454c4ee6fa3
SaiDurga-28 <saidurgaanisetti9@gmail.com>
2025-09-21 17:37:15 +0530

    UseStateArrayExample

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Component1 from './Component1'
 import Component2 from './Component2'
 import Component from './Component'
 import Hook_1_Task_Cards from './Hook_1_Task_Cards'
+import UseStateArrayExample from './UseStateArrayExample'
 // import Hook_useState from './Hook_useState'
 // import FunctionalComponent from './FunctionComponent.jsx'
 // import FormComponent from './FormComponent'
@@ -87,7 +88,8 @@ function App() {
 } */}
 {/* <Component/> */}
 {/* <Hook_useState /> */}
-<Hook_1_Task_Cards/>
+{/* <Hook_1_Task_Cards/> */}
+<UseStateArrayExample/>
     </>
   )
 }
---
+++ src/UseStateArrayExample.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+function UseStateArrayExample(){
+    const [data,setdata]=useState([1,2,3])
+    const change=()=>{
+        setdata([4,5,6,7,8])
+    }
+    return(
+        <>
+            {
+                data.map(ele=>{
+                    return <h1>{ele}</h1>
+                })
+            }
+            <button onClick={change}>ClickToChange</button>
+        </>
+    )
+}
+export default UseStateArrayExample
