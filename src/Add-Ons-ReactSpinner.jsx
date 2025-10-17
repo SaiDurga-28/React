880971cb1c9a44a880ceb38069c0c6b852b3135a
SaiDurga-28 <saidurgaanisetti9@gmail.com>
2025-10-17 10:50:04 +0530

    SpinnerOrLoaderAddOn

---
+++ src/Add-Ons-ReactSpinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { RotatingLines } from "react-loader-spinner";
+function AddOn_Spinner(){
+     return(
+        <>
+        <h1>Loading</h1>
+        <RotatingLines/>
+        </>
+     )
+}
+export default AddOn_Spinner
