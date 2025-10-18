b10b5a377bfaf9224a7bef6f7866634aacaf70e9
SaiDurga-28 <saidurgaanisetti9@gmail.com>
2025-10-18 13:36:08 +0530

    ConfettiAddOn

---
+++ src/Add-Ons-ReactConfetti.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Confetti from "react-confetti";
+function AddOn_Confetti(){
+     return(
+        <>
+        <h1>Congratulations
+       </h1>
+       <Confetti
+       numberOfPieces={300}
+    //    gravity={20}
+       />
+        </>
+     )
+}
+export default AddOn_Confetti
